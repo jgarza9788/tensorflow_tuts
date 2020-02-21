09d499e0941ca376e36e1d9652abdeaf6fc92e82
jgarza9788 <jgarza9788@gmail.com>
2020-02-20 16:18:32 -0800

    just messing around

---
+++ tensorflow_weather_v2.py
@@ -0,0 +1,185 @@
+# https://www.tensorflow.org/tutorials/structured_data/time_series
+
+#import some libs
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from datetime import datetime
+
+mpl.rcParams['figure.figsize'] = (8, 6)
+mpl.rcParams['axes.grid'] = False
+
+dir = os.path.dirname(os.path.realpath(__file__))
+modelDir = os.path.join(dir,'model')
+
+
+
+
+# print(modelDir)
+# print(os.path.join(modelDir,'modelName.h5'))
+# quit()
+
+#get the data
+zip_path = tf.keras.utils.get_file(
+    origin='https://storage.googleapis.com/tensorflow/tf-keras-datasets/jena_climate_2009_2016.csv.zip',
+    fname='jena_climate_2009_2016.csv.zip',
+    extract=True)
+csv_path, _ = os.path.splitext(zip_path)
+
+#for mat into dataframe
+df = pd.read_csv(csv_path)
+df.head()
+
+def univariate_data(dataset, start_index, end_index, history_size, target_size):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i)
+        # Reshape data from (history_size,) to (history_size, 1)
+        data.append(np.reshape(dataset[indices], (history_size, 1)))
+        labels.append(dataset[i+target_size])
+    return np.array(data), np.array(labels)
+
+TRAIN_SPLIT = 300000
+tf.random.set_seed(13)
+
+features_considered = ['p (mbar)', 'T (degC)', 'rho (g/m**3)']
+features = df[features_considered]
+features.index = df['Date Time']
+features.head()
+
+features.plot(subplots=True)
+
+dataset = features.values
+data_mean = dataset[:TRAIN_SPLIT].mean(axis=0)
+data_std = dataset[:TRAIN_SPLIT].std(axis=0)
+
+dataset = (dataset-data_mean)/data_std
+
+
+def multivariate_data(dataset, target, start_index, end_index, history_size,
+                        target_size, step, single_step=False):
+    data = []
+    labels = []
+
+    start_index = start_index + history_size
+    if end_index is None:
+        end_index = len(dataset) - target_size
+
+    for i in range(start_index, end_index):
+        indices = range(i-history_size, i, step)
+        data.append(dataset[indices])
+
+        if single_step:
+            labels.append(target[i+target_size])
+        else:
+            labels.append(target[i:i+target_size])
+
+    return np.array(data), np.array(labels)
+
+BATCH_SIZE = 256
+BUFFER_SIZE = 10000
+past_history = 720
+future_target = 72
+STEP = 6
+EVALUATION_INTERVAL = 200
+EPOCHS = 10
+
+
+x_train_multi, y_train_multi = multivariate_data(dataset, dataset[:, 1], 0,
+                                                    TRAIN_SPLIT, past_history,
+                                                    future_target, STEP)
+x_val_multi, y_val_multi = multivariate_data(dataset, dataset[:, 1],
+                                                TRAIN_SPLIT, None, past_history,
+                                                future_target, STEP)
+
+print ('Single window of past history : {}'.format(x_train_multi[0].shape))
+print ('\n Target temperature to predict : {}'.format(y_train_multi[0].shape))
+
+train_data_multi = tf.data.Dataset.from_tensor_slices((x_train_multi, y_train_multi))
+train_data_multi = train_data_multi.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
+val_data_multi = tf.data.Dataset.from_tensor_slices((x_val_multi, y_val_multi))
+val_data_multi = val_data_multi.batch(BATCH_SIZE).repeat()
+
+def create_time_steps(length):
+    return list(range(-length, 0))
+
+def multi_step_plot(history, true_future, prediction):
+    plt.figure(figsize=(12, 6))
+    num_in = create_time_steps(len(history))
+    num_out = len(true_future)
+
+    plt.plot(num_in, np.array(history[:, 1]), label='History')
+    plt.plot(np.arange(num_out)/STEP, np.array(true_future), 'bo',
+            label='True Future')
+    if prediction.any():
+        plt.plot(np.arange(num_out)/STEP, np.array(prediction), 'ro',
+                label='Predicted Future')
+    plt.legend(loc='upper left')
+    plt.show()
+
+for x, y in train_data_multi.take(1):
+    multi_step_plot(x[0], y[0], np.array([0]))
+
+
+
+multi_step_model = tf.keras.models.Sequential()
+multi_step_model.add(tf.keras.layers.LSTM(32,
+                                            return_sequences=True,
+                                            input_shape=x_train_multi.shape[-2:]))
+multi_step_model.add(tf.keras.layers.LSTM(16, activation='relu'))
+multi_step_model.add(tf.keras.layers.Dense(72))
+
+multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(clipvalue=1.0), loss='mae')
+
+
+# multi_step_history = multi_step_model.fit(train_data_multi, epochs=EPOCHS,
+#                                             steps_per_epoch=EVALUATION_INTERVAL,
+#                                             validation_data=val_data_multi,
+#                                             validation_steps=50)
+
+
+# dt = datetime.now().strftime('%Y%m%d')
+# modelName = __file__.split('\\')[-1:][0].replace('.py', '') + dt + '.h5'
+# print(modelName)
+# print(os.path.join(modelDir,modelName))
+# multi_step_model.save(os.path.join(modelDir,modelName)) 
+# quit()
+
+multi_step_model = tf.keras.models.load_model(r'c:\Users\JGarza\GitHub\tensorflow_tuts\model\tensorflow_weather_v2.py20200219.h5')
+
+for x, y in val_data_multi.take(1):
+    print (multi_step_model.predict(x).shape)
+
+def plot_train_history(history, title):
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(loss))
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'b', label='Training loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation loss')
+    plt.title(title)
+    plt.legend()
+
+    plt.show()
+
+# plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
+
+for x, y in val_data_multi.take(3):
+    multi_step_plot(x[0], y[0], multi_step_model.predict(x)[0])
+
+input()
